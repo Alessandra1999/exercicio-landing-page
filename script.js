e0159a3dda0d6106294169e0710911bebd182648
Alessandra Bombardi <ale.brbr@gmail.com>
2024-06-17 17:38:05 -0300

    teste 2

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function enviar() {
-    alert("Agradecemos o seu contato, responderemos em breve (confia)!")
+    alert("Agradecemos o seu contato, responderemos em breve!");
 }
 
 function saibaMais() {
     window.open('https://studioghibli.com.br/studioghibli/');
 }
 
+//slider 1
 const controls = document.querySelectorAll('.control');
 
 let currentItem = 0;
@@ -38,5 +39,41 @@ controls.forEach(control => {
         });
 
         items[currentItem].classList.add('current-item');
+    });
+});
+
+//slider 2
+const controls2 = document.querySelectorAll('.control2');
+
+let currentItem2 = 0;
+const items2 = document.querySelectorAll('.item2');
+const maxItems2 = items2.length;
+
+controls2.forEach(control2 => {
+    control2.addEventListener('click', () => {
+        const isLeft2 = control2.classList.contains('arrow-left2');
+
+        if (isLeft2) {
+            currentItem2 -= 1;
+        } else {
+            currentItem2 += 1;
+        }
+
+        if (currentItem2 >= maxItems2) {
+            currentItem2 = 0;
+        }
+
+        if (currentItem2 < 0) {
+            currentItem2 = maxItems2 -1;
+        }
+
+        items2.forEach(item2 => item2.classList.remove('current-item2'));
+
+        items2[currentItem2].scrollIntoView({
+            inline: "center",
+            behavior: "smooth",
+        });
+
+        items2[currentItem2].classList.add('current-item2');
     });
 });
